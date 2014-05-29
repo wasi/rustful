808c9bd81b701bd961d39d3f736202078c5e9fa2
Travis <travis@travis-ci.org>
2014-05-29 11:31:26 +0000

    Update docs from Travis build 55

---
+++ doc/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rustful'] = {Server:"rustful/server/struct.Server.html",Router:"rustful/router/struct.Router.html",};
+
+                if (window.register_implementors) {
+                    window.register_implementors(implementors);
+                } else {
+                    window.pending_implementors = implementors;
+                }
+            
+})()
---
+++ doc/implementors/std/io/trait.Writer.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rustful'] = {Response:"rustful/response/struct.Response.html",};
+
+                if (window.register_implementors) {
+                    window.register_implementors(implementors);
+                } else {
+                    window.pending_implementors = implementors;
+                }
+            
+})()
