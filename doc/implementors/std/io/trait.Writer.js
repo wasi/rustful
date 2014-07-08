d1aca8314a49fcffd0be12718d65ea0d7ae60a39
Travis <travis@travis-ci.org>
2014-07-08 22:11:17 +0000

    Update docs from Travis build 74

--- doc/implementors/std/io/trait.Writer.js
+++ doc/implementors/std/io/trait.Writer.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustful'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, 'b&gt; <a class='trait' href='http://doc.rust-lang.org/std/io/trait.Writer.html' title='std::io::Writer'>Writer</a> for <a class='struct' href='rustful/struct.Response.html' title='rustful::Response'>Response</a>&lt;'a, 'b&gt;",];
+implementors['rustful'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, 'b&gt; <a class='trait' href='http://doc.rust-lang.org/0.11.0/std/io/trait.Writer.html' title='std::io::Writer'>Writer</a> for <a class='struct' href='rustful/struct.Response.html' title='rustful::Response'>Response</a>&lt;'a, 'b&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
