f579a27a3bd8170db3637b6b7bd501d675e2db7f
Travis <travis@travis-ci.org>
2014-12-13 16:45:09 +0000

    Update docs from Travis build 99

---
+++ doc/implementors/http/server/trait.Server.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rustful'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;R, H, C&gt; <a class='trait' href='http://www.rust-ci.org/chris-morgan/rust-http/doc/http/server/trait.Server.html' title='http::server::Server'>Server</a> for <a class='struct' href='rustful/struct.ServerInstance.html' title='rustful::ServerInstance'>ServerInstance</a>&lt;R, C&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
