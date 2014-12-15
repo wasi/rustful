a1b7f1653f9ea8d76d6416ea847c168d9c8a3b30
Travis <travis@travis-ci.org>
2014-12-15 06:57:04 +0000

    Update docs from Travis build 106

--- doc/implementors/core/default/trait.Default.js
+++ doc/implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustful'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;R, C&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rustful/struct.Server.html' title='rustful::Server'>Server</a>&lt;R, C&gt;",];
+implementors['rustful'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;R, C&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rustful/struct.Server.html' title='rustful::Server'>Server</a>&lt;R, C&gt;","<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for RedirectPolicy",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
